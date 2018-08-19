075abfbaf2e71be149f24f87db11bae87c151221
Nicholas Thanasiu <nick.thanasiu@gmail.com>
2018-08-18 20:20:11 -0400

    Create Sign Up Page Component

---
+++ src/components/SignupPage/SignupPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SignupPage = () => (
+  <div className="signup-page">
+    <h5>
+      Sign up
+    </h5>
+    <div className="signup-form-container">
+      SignupForm appears here
+    </div>
+  </div>
+);
+
+export default SignupPage;
